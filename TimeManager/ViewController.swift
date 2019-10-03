a316644f9329076dd45c64d6202d728b1feb3fad
Oswaldo Maestra <omaestra@viewnext.com>
2019-10-03 17:01:16 +0200

    Initial Commit

---
+++ TimeManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeManager
+//
+//  Created by Oswaldo Maestra on 03/10/2019.
+//  Copyright © 2019 omaestra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
